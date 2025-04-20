3f4bcd48d29d6a9e74a50879932b67dbc3e956bf
mdp3042005 <mahmoud.d.p7@gmail.com>
2025-04-20 21:37:12 +0330

    Add radius token

---
+++ mdpTailwindPlugin/tokens/radiusTokens.ts
@@ -0,0 +1,10 @@
+/* eslint-disable perfectionist/sort-objects */
+export const radius = {
+  none: '0px',
+  xs: '4px',
+  sm: '8px',
+  md: '12px',
+  lg: '16px',
+  xl: '28px',
+  full: '9999px',
+} as const;
