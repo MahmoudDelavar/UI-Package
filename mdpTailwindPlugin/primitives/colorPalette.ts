1d03d0433a4ed2f8d7b44491a90fa40cbfa52dcb
mdp3042005 <mahmoud.d.p7@gmail.com>
2025-04-20 19:26:58 +0330

    Add color pallet

---
+++ mdpTailwindPlugin/primitives/colorPalette.ts
@@ -0,0 +1,51 @@
+/* eslint-disable perfectionist/sort-objects */
+export const colorPalette = {
+  neutral: {
+    50: '#F8FAFC',
+    100: '#F3F4F6',
+    200: '#E5E7EB',
+    300: '#CBD5E1',
+    400: '#94A3B8',
+    500: '#6B7280',
+    600: '#4B5563',
+    700: '#374151',
+    800: '#1F2937',
+    900: '#111827',
+  } as const,
+
+  white: '#ffffff',
+
+  black: '#000000',
+
+  rose: {
+    50: '#FFF1F2',
+    300: '#de6a70',
+    400: '#FB7185',
+    600: '#E11D48',
+  } as const,
+
+  amber: {
+    100: '#FEF3C7',
+    400: '#F59E0B',
+    600: '#D97706',
+  } as const,
+
+  brand: {
+    50: '#EEF2FF',
+    100: '#E0E7FF',
+    200: '#C7D2FE',
+    300: '#A5B4FC',
+    400: '#818CF8',
+    500: '#6366F1',
+    600: '#4F46E5',
+    700: '#4338CA',
+    800: '#3730A3',
+    900: '#312E81',
+  } as const,
+
+  teal: {
+    50: '#F0FDFA',
+    400: '#2DD4BF',
+    600: '#0D9488',
+  } as const,
+};
