df3ee05684ce9f21c1876668645ce36ba555a72a
mdp3042005 <mahmoud.d.p7@gmail.com>
2025-04-20 21:36:51 +0330

    Add break point token

---
+++ mdpTailwindPlugin/tokens/breakpointTokens.ts
@@ -0,0 +1,10 @@
+/* eslint-disable perfectionist/sort-objects */
+
+export const BreakpointTokens = {
+  xs: '360px',
+  sm: '576px',
+  md: '768px',
+  lg: '992px',
+  xl: '1400px',
+  xxl: '1536px',
+} as const;
