5531e73301b3d64777ef19499f8500d730bf8f82
mdp3042005 <mahmoud.d.p7@gmail.com>
2025-04-20 21:50:02 +0330

    Improve color token

--- mdpTailwindPlugin/tokens/colorToken.ts
+++ mdpTailwindPlugin/tokens/colorToken.ts
@@ -1,26 +1,6 @@
 /* eslint-disable perfectionist/sort-objects */
 import { colorPalette as c } from '../primitives';
 
-export const ColorTokens = {
-  background: {
-    disable: c.neutral[100],
-  },
-
-  text: {
-    body: c.neutral[800],
-    subtle: c.black[200],
-    inverse: c.white[100],
-    disabled: c.neutral[400],
-    brand: c.neutral[500],
-  },
-
-  border: {
-    DEFAULT: c.neutral[300],
-    default: c.neutral[300],
-    disabled: c.neutral[400],
-  },
-};
-
 export const variation = {
   primary: {
     DEFAULT: c.neutral[600],
@@ -58,5 +38,32 @@ export const variation = {
     pressed: c.teal[600],
     focused: c.teal[600],
     disabled: c.teal[50],
+  },
+};
+
+export const ColorTokens = {
+  background: {
+    surface: {
+      disable: c.neutral[100],
+      danger: c.rose[400],
+      warning: c.amber[400],
+    },
+    ...variation,
+  },
+
+  text: {
+    body: c.neutral[800],
+    subtle: c.black[200],
+    inverse: c.white[100],
+    disabled: c.neutral[400],
+    brand: c.neutral[500],
+    ...variation,
+  },
+
+  border: {
+    DEFAULT: c.neutral[300],
+    default: c.neutral[300],
+    disabled: c.neutral[400],
+    ...variation,
   },
 };
