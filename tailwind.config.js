bc885ef42b1bc8d7363fd531ff6358a1ceb0403f
mdp3042005 <mahmoud.d.p7@gmail.com>
2025-04-21 12:56:18 +0330

    improve styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
+
+import { mdpTailwindPlugin } from './mdpTailwindPlugin';
+
 export default {
   content: ['./src/**/*.{ts,tsx}', '.storybook/*.ts'],
-  plugins: [],
+  plugins: [mdpTailwindPlugin()],
 };
